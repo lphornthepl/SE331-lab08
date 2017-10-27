2ea71b1fdde8e8b9a23f01c7b5865ac15201968e
Phornthep <phornthep_chooleat@cmu.ac.th>
2017-10-27 16:13:48 +0700

    No.5.4

--- src/main/java/camt/cbsd/lab05/entity/Student.java
+++ src/main/java/camt/cbsd/lab05/entity/Student.java
@@ -1,34 +1,33 @@
 package camt.cbsd.lab05.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 
 @Entity
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = false)
-
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
 public class Student {
+    @ManyToMany
+    List<Course> enrolledCourse;
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
-    long id;
-    String studentId;
-    String name;
-    String surname;
-    double gpa;
-    String image;
-    boolean feature;
-    int penAmount;
-    String description;
+    @NonNull long id;
+    @NonNull String studentId;
+    @NonNull String name;
+    @NonNull String surname;
+    @NonNull double gpa;
+    @NonNull String image;
+    @NonNull boolean feature;
+    @NonNull int penAmount;
+    @NonNull String description;
 
 //    public String getName() {
 //        return name;
